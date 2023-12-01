5d60b2074065ffe9b11aaf3cd541cfd267861249
ashtilawat23 <ashtilawat23@gmail.com>
2023-12-01 13:06:26 -0500

    added game data validation

--- app/validation.py
+++ app/validation.py
@@ -5,3 +5,10 @@ from pydantic import BaseModel
 class Question(BaseModel):
     question: str
     word: str
+
+
+class GameData(BaseModel):
+    word: str
+    questions: list
+    guessed_correctly: bool
+    additional_data: dict = {}  # Any additional data you want to store
